7ca11fbcaaaef12022235b3f818017830fbf7ee6
leiz2 <leiz2@jumei.com>
2017-09-30 16:32:35 +0800

    修改异常

--- app/Http/Controllers/MockController.php
+++ app/Http/Controllers/MockController.php
@@ -20,7 +20,7 @@ class MockController extends AHCController
             1 => json_encode(array('user' => 'dki')),
         ];
 
-        $rep = $list[$id];
+        $rep = !empty($list) && isset($list[$id]) ? $list[$id] : 'invalid mock_id or list is empty';
         $response = new Response($rep, 200);
         // $response->withCookie('user', md5($id), 3600, '/', 'lumen.com');
         return $response;
